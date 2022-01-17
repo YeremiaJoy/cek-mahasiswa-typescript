f84e0a1ed8e2f44036b204294abb7d95e244ec88
YeremiaJoy <yeremiajoy@gmail.com>
2022-01-17 10:43:00 +0700

    delete width

--- src/styles/_emoticon.tsx
+++ src/styles/_emoticon.tsx
@@ -5,13 +5,12 @@ export const HomeLayout = styled.div`
 position: absolute;
 top: 50%; right: 50%;
 transform: translate(50%, -50%);
-padding: 50px 50px;
+padding: 50px;
 margin: 2px;
 border-radius: 9px;
 border: 1px solid rgb(234, 234, 234);
 overflow: hidden;
 box-shadow: rgb(49 53 59 / 12%) 0px 1px 6px 0px;
-width: 500px;
 max-width: 700px;
 `
 
